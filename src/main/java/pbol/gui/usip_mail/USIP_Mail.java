3f0ad41ca601966fdb262e4ef41c119fbcaff514
narwa <narwandaru.nara@gmail.com>
2023-11-30 13:33:08 +0700

    initial commit

---
+++ src/main/java/pbol/gui/usip_mail/USIP_Mail.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pbol.gui.usip_mail;
+
+/**
+ *
+ * @author narwa
+ */
+public class USIP_Mail {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
